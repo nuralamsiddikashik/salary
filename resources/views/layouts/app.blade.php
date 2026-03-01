1c855fd1a6b8e3dea3836c48571a0dace5768209
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-03-01 23:07:57 +0600

    Update code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <title>{{ config('app.name', 'Salary System') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
